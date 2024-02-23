6783ca7374e803aeb7749e4f6aa08d7c660798ad
kimsseTheWolf <ilovemcmrcreeper@gmail.com>
2024-02-22 22:05:45 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,94 @@
+<script setup>
+import { Dropdown, Menu, MenuItem, Button, Input } from 'ant-design-vue';
+import {
+  UploadOutlined, 
+  SaveOutlined, 
+  ExportOutlined, 
+  CopyOutlined, 
+  PaperClipOutlined,
+  EditOutlined
+} from '@ant-design/icons-vue'
+import { ref } from 'vue';
+import MenuBtn from "./components/MenuBtn.vue"
+
+const fileName = ref("")
+const interfaceControl = ref({
+  showFileNameInput: false
+})
+
+</script>
+
+<template>
+  <div class="main-window">
+    <div class="title-bar">
+      <div class="column item">
+        <div class="row">
+          <div class="file-name item">
+            <div v-if="!interfaceControl.showFileNameInput">{{ fileName === "" ? "New File" : fileName }}</div>
+            <Input v-if="interfaceControl.showFileNameInput" placeholder="New File Name" v-model:value="fileName"/>
+          </div>
+          <Button type="primary" size="small" @click="interfaceControl.showFileNameInput = !interfaceControl.showFileNameInput">
+            <EditOutlined/>
+            Change
+          </Button>
+        </div>
+        <div class="row">
+          <Dropdown>
+            <MenuBtn>File</MenuBtn>
+            <template #overlay>
+              <Menu>
+                <MenuItem key="1">
+                  <UploadOutlined/>
+                  Upload from computer
+                </MenuItem>
+                <MenuItem key="2">
+                  <SaveOutlined/>
+                  Save
+                </MenuItem>
+                <MenuItem key="3">
+                  <ExportOutlined/>
+                  Export as Metacode file
+                </MenuItem>
+                <MenuItem key="4">
+                  <ExportOutlined/>
+                  Export as Python file
+                </MenuItem>
+              </Menu>
+            </template>
+          </Dropdown>
+          <Dropdown>
+            <MenuBtn>Edit</MenuBtn>
+            <template #overlay>
+              <Menu>
+                <MenuItem key="1">
+                  <CopyOutlined/>
+                  Copy
+                </MenuItem>
+                <MenuItem key="2">
+                  <PaperClipOutlined/>
+                  Paste
+                </MenuItem>
+              </Menu>
+            </template>
+          </Dropdown>
+          <Dropdown>
+            <MenuBtn>Tools</MenuBtn>
+          </Dropdown>
+          <Dropdown>
+            <MenuBtn>Others</MenuBtn>
+          </Dropdown>
+        </div>
+      </div>
+    </div>
+
+    <div class="main-body">
+      <div class="block-panel panel"></div>
+      <div class="work-panel panel"></div>
+      <div class="code-panel panel"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/MenuBtn.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+<template>
+<div class="menu-button">
+    <slot></slot>
+</div>
+</template>
+<style>
+.menu-button {
+  margin-right: 5px;
+  padding: 5px;
+  border-radius: 5px;
+  width: fit-content;
+  height: fit-content;
+  cursor: pointer;
+  transition: all 0.1s;
+  font-size: 15px;
+}
+.menu-button:hover {
+  background-color: rgba(0, 0, 0, 0.1);
+}
+</style>
